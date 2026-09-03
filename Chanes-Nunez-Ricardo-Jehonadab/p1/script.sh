4bc3425c86ad8cf0e46a2bb1080abe7989e49965
Ricardo Chanes <chanesss123@gmail.com>
2026-09-03 16:31:20 -0600

    feature: p1

---
+++ Chanes-Nunez-Ricardo-Jehonadab/p1/script.sh
@@ -0,0 +1,21 @@
+function log {
+    >&2 echo "$1"
+}
+function crawl {
+dirs=$(find -s $1 -depth 1 -type d | sort -r)
+prev=""
+for dir in $dirs; do
+    #crawl $dir
+    prev=$(crawl $dir)
+done
+output=$(ls -lhT $1)
+hash1=$(echo $output | md5)
+hash2=$(echo "$hash1$prev" | md5)
+log "======= $1 :: $hash1 :: $hash2 ========"
+log $prev
+log "=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+"
+log $output
+echo $hash2
+}
+
+crawl .
